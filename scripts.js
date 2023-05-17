5c6dea18a9491a8c85f0e173f945540a2697da3a
Keago99 <keatabde@gmail.com>
2023-05-17 16:19:38 +0200

    Added div function and color testing.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,37 @@
-let board = document.querySelector(".board");
-// Creates a board that has 16 columns, and the width of each colum is 1/16th of the total width.
-board.style.gridTemplateColumns = "repeat(16 , 1fr)";
-board.style.gridTemplateRows = "repeat(16 , 1fr)";
+function populateBoard(size){
+    let board = document.querySelector(".board");
+    let squares = document.querySelectorAll(".div");
+    // Creates and executes a simple function that removes all divs before appending them
+    squares.forEach((div) => div.remove());
 
-for(let i = 0; i<256; i++){
-    let square = document.createElement('div');
-    square.style.backgroundColor = "green";
-    board.insertAdjacentElement('beforeend', square);
+    // Creates a Board with columns and rows equal to the size of the "size" variable
+    // Each block will be one size variable.
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    // After columns and rows are set, we need to set a variable for the amount.
+    let amount = size*size;
+    // For loop that creates a Div until board is full, each div is colored green
+    for(let i = 0; i<amount; i++){
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {this.style.backgroundColor = "black"});
+        square.style.backgroundColor = "green";
+        board.insertAdjacentElement('beforeend', square);
+    }
 }
+
+// Basically just calling the populateBoard function but with user input taken from DOM.
+function changeSize(input){
+    // Validate input so the program doesn't crash on large inputs and doesn't receive
+    // negative numbers.
+    if (input >= 2 && input <= 100){
+        populateBoard(input);
+    }
+    else{
+        console.log("Too many squares");
+    }
+    
+
+}
+
+populateBoard(32);
